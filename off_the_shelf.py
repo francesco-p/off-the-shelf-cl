2ef4e24c126a0d1fd74c4670bf32b7e895bf79b6
francesco-p <pelosinf@gmail.com>
2021-11-23 17:47:50 +0100

    adds all

--- off_the_shelf.py
+++ off_the_shelf.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn as nn
 import torchvision
 from continuum import ClassIncremental
-from continuum.datasets import MNIST, CIFAR10, CIFAR100, Core50
+from continuum.datasets import MNIST, CIFAR10, CIFAR100, Core50, CUB200
 from torch.utils.data import DataLoader
 from torchvision.transforms import transforms
 from einops import rearrange
@@ -14,20 +14,45 @@ import random
 import timm
 import pickle
 
+CUDA_DEVICE = 'cuda:2'
+
+def compute_prototype(x, model, bs, cuda=True):
+    device = CUDA_DEVICE if cuda else 'cpu'
+
+    n = x.shape[0]
+    iters = n // bs
+    features = None
+    for i in range(iters):
+        init = i * bs
+        end = init + bs
+        out = model(x[init : end].to(device)).cpu()
+        if features == None:
+            features = out
+        else:
+            features = torch.cat((features, out), dim=0)
+
+    if end < n:
+        out = model(x[end:].to(device)).cpu()
+        features = torch.cat((features, out), dim=0)
+
+    return features.mean(dim=0)
 
 
-
-def get_prototype(x, pretr_models, cuda=True):
+def get_prototype(x, pretr_models, test=False, cuda=True):
     """ Forward data to the models and create a prototype (mean of the features) """
     with torch.no_grad():
-        device = 'cuda' if cuda else 'cpu'
-        
+        device = CUDA_DEVICE if cuda else 'cpu'
         prototype = []
         for model in pretr_models:
             model.eval()
             model.to(device)
             x = x.to(device)
-            prototype.append(model.forward_features(x).mean(dim=0).view(-1).cpu())
+
+            if test:
+                prototype.append(model(x).mean(dim=0).view(-1).cpu())
+            else:
+                prototype.append(compute_prototype(x, model, 256))
+
             model.to('cpu')
             x.to('cpu')
     
@@ -36,7 +61,7 @@ def get_prototype(x, pretr_models, cuda=True):
 
 def prototype_distance(prototype1, prototype2, cuda=True):
     """ Forward data to the models and create a prototype """
-    device = 'cuda' if cuda else 'cpu'
+    device = CUDA_DEVICE if cuda else 'cpu'
 
     tot_dist = 0.
     for s1, s2 in zip(prototype1, prototype2):
@@ -68,7 +93,8 @@ def get_closest_prototype(memory, prototype2, cuda=True):
 DATA_PATH = "~/data"
 SEED = 0
 CUDA = True
-MEMORY_FNAME = './pkls/memory_resnet152.pkl'
+MEMORY_FNAME = './pkls/CUB200_memory_resnet_50.pkl'
+MODEL = 'resnet50'
 
 # Reproducibility seeds
 torch.manual_seed(SEED)
@@ -82,10 +108,11 @@ np.random.seed(SEED)
 # You can combine more than one feature extractor
 # all models pretrined in imagenet available here: timm.list_models()
 
-names = ['resnet152'] 
+names = [MODEL] 
 models = []
 for name in names:
-    model =  timm.create_model(name, pretrained=True)
+    model =  timm.create_model(name, pretrained=True, num_classes=0)
+    #model =  torchvision.models.resnet152(pretrained=True)
     model.to('cpu')
     models.append(model) 
 
@@ -97,9 +124,10 @@ for name in names:
 ### Create Prototype Memory ###
 ###############################
 
+memory = {}
 # Task split in each class (corresponds to increment==1)
 tr_scenario = ClassIncremental(
-    CIFAR100(data_path=DATA_PATH, download=True, train=True),
+    CUB200(data_path=DATA_PATH, download=True, train=True),
     increment=1,
     transformations=[
                      transforms.Resize(224), #Imagenet original data size
@@ -107,7 +135,6 @@ tr_scenario = ClassIncremental(
                      transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))]
 )
 
-memory = {}
 for task_id, tr_taskset in enumerate(tr_scenario):
     #if task_id < 50:
         #continue
@@ -133,7 +160,7 @@ with open(MEMORY_FNAME, 'wb') as f:
 ############
 
 te_scenario = ClassIncremental(
-    CIFAR100(data_path=DATA_PATH, download=True, train=False),
+    CUB200(data_path=DATA_PATH, download=True, train=False),
     increment=1,
     transformations=[
                     transforms.Resize(224),
@@ -152,17 +179,17 @@ for task_id, te_taskset in enumerate(te_scenario):
 
     for x, y, t in DataLoader(te_taskset, batch_size=1):
         n+= 1
-        x_sign = get_prototype(x, models, cuda=CUDA)
+        x_sign = get_prototype(x, models, test=True, cuda=CUDA)
 
         pred = get_closest_prototype(memory, x_sign, cuda=True)
         
         if pred == y.item():
             tot += 1
-        print(tot, n, pred, y)
+        print(tot, n, pred, y.item(), f"{(tot/n):.3f}")
         rel_mat[y.item(), pred] += 1
 
 # correct predictions, total elements
-print(tot, n)
+print('CUB200-RESNET50',tot, n)
 plt.imshow(rel_mat)
-plt.savefig("../cacherepo/resnet152.png")
-import ipdb; ipdb.set_trace()
+plt.savefig(f"./pngs/CUB_200_{MODEL}.png")
+#import ipdb; ipdb.set_trace()
